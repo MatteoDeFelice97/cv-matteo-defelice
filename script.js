9cdfa1f52c5ff8cbfea3309937ca1749f183478c
MatteoDeFelice97 <matteo.defe97@gmail.com>
2025-03-08 00:13:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function toggleSection(id) {
-    var section = document.getElementById(id);
-    section.style.display = (section.style.display === "none") ? "block" : "none";
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    
+    tablinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+    
+    document.getElementById(tabName).style.display = "block";
+    document.getElementById(tabName).classList.add("active");
+    evt.currentTarget.classList.add("active");
 }
